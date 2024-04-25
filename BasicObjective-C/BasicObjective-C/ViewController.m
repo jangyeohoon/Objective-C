588cdebda7ea3ca2f1503c9fe9ca8bafc60362ff
jangyeohoon <someday0307@naver.com>
2024-04-25 23:46:05 +0900

    Update ViewController.m

--- BasicObjective-C/BasicObjective-C/ViewController.m
+++ BasicObjective-C/BasicObjective-C/ViewController.m
@@ -17,10 +17,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    SEL btnSelector = @selector(onTestBtnClicked:);
+    
     Friend *createdFriend = [Friend getAFriend];
     
     NSLog(@"createdFriend : %@", [createdFriend nickName]);
 }
 
+- (void)onTestBtnClicked:(UIButton *) sender {
+    NSLog(@"name: %@, btnTitle: %@",
+          NSStringFromSelector(_cmd), sender.titleLabel.text);
+}
+
+/// 매개변수 없고, 반환값 또한 없다
+/// [self sayHello];
+- (void)sayHello {
+    NSLog(@"안녕하세요!");
+}
+
+- (int)getRandomNumber {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    /// 0~99 무작위 수
+    return arc4random_uniform(100);
+}
 
 @end
